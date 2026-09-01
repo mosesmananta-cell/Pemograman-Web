da6b433986258be584cc92bb1b001f7204976ed3
mosesmananta-cell <mosesmananta@students.unnes.ac.id>
2026-09-02 02:18:21 +0700

    Add files via upload

---
+++ dynamicTyping.js
@@ -0,0 +1,10 @@
+let data = "true";
+console.log(data, "-> Tipenya:", typeof data);
+
+// Mengubah tipe data ke angka (number)
+data = 400;
+console.log(data, "-> Tipenya:", typeof data);
+
+// Mengubah tipe data ke boolean (true/false)
+data = false;
+console.log(data, "-> Tipenya:", typeof data);
